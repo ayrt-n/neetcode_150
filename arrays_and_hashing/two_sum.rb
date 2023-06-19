78062831de77e24606fc44456347d845c3c137e1
ayrton <ayrton.parkinson1@gmail.com>
2023-06-19 07:21:11 -0400

    Add better solution using hash

--- arrays_and_hashing/two_sum.rb
+++ arrays_and_hashing/two_sum.rb
@@ -14,3 +14,20 @@ def bf_two_sum(nums, target)
     end
   end
 end
+
+# Better solution, using hash
+# Time complexity: O(n)
+# Space complexity: O(n)
+def two_sum(nums, target)
+  nums_hash = {}
+
+  nums.each_with_index do |n1, i1|
+    solution = target - n1
+    i2 = nums_hash[solution]
+    return [i1, i2] if i2
+
+    nums_hash[n1] = i1
+  end
+
+  nil
+end
