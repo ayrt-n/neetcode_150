86a33950e59c13b3b56e65ec17d787d0e7e3f881
ayrton <ayrton.parkinson1@gmail.com>
2023-06-22 07:14:09 -0400

    Add bfs solution

--- trees/maximum_depth_binary_tree.rb
+++ trees/maximum_depth_binary_tree.rb
@@ -23,10 +23,10 @@ def max_depth(root)
   [left_depth, right_depth].max + 1
 end
 
-# Iterative Solution
+# Depth-First Iterative Solution
 # Time complexity: O(n)
-# Space complexity: 
-def max_depth_loop(root)
+# Space complexity: O(n)
+def max_depth_dfs(root)
   result = 0
   return result if root.nil?
 
@@ -41,4 +41,26 @@ def max_depth_loop(root)
   end
 
   result
+end
+
+# Breadth-First Iterative Solution
+# Time complexity: O(n)
+# Space complexity: O(n)
+def max_depth_bfs(root)
+  return 0 if root.nil?
+
+  level = 0
+  queue = [root]
+
+  until queue.empty?
+    queue.length.times do
+      node = queue.shift
+      queue.push(node.left) if node.left
+      queue.push(node.right) if node.right
+    end
+
+    level += 1
+  end
+
+  level
 end
