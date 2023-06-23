653413b7e4a891be025d3678f30fd325871d21fb
ayrton <ayrton.parkinson1@gmail.com>
2023-06-23 07:04:56 -0400

    Add same_tree solution

---
+++ trees/same_tree.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# Definition for a binary tree node
+class TreeNode
+  attr_accessor :val, :left, :right
+
+  def initialize(val = 0, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+end
+
+def same_tree?(p, q)
+  p_queue = [p]
+  q_queue = [q]
+
+  until p_queue.empty? || q_queue.empty?
+    p_node = p_queue.shift
+    q_node = q_queue.shift
+    return false if p_node&.val != q_node&.val
+
+    p_queue.push(p_node.left, p_node.right) if p_node
+    q_queue.push(q_node.left, q_node.right) if q_node
+  end
+
+  p_queue.length == q_queue.length
+end
