05c01b07857d080f461b439a745a622a4db151ae
ayrton <ayrton.parkinson1@gmail.com>
2023-06-25 11:03:00 -0400

    Add climbing_stairs solution

--- dynamic_programming/climbing_stairs.rb
+++ dynamic_programming/climbing_stairs.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# Time complexity: O(n)
+# Space complexity: O(n)
 def climb_stairs(n)
-  
+  steps = Array.new(n + 2)
+  steps[0], steps[1] = 0, 1
+
+  (2...n + 2).each do |i|
+    steps[i] = steps[i - 1] + steps[i - 2]
+  end
+
+  steps[-1]
 end
