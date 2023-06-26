4659da3e4ea929fea564716c5fb23b65ca0096e9
ayrton <ayrton.parkinson1@gmail.com>
2023-06-26 07:32:46 -0400

    Add non-sorting solution

--- arrays_and_hashing/group_anagrams.rb
+++ arrays_and_hashing/group_anagrams.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
-# Time complexity: O(n * w * logw) where n is the number of words and w is the average characters per word
+# Time complexity: O(n * w) where n is the number of words and w is the average characters per word
 # Space complexity: O(n)
 def group_anagrams(strs)
   results = Hash.new { |h, k| h[k] = [] }
 
   strs.each do |str|
-    key = anagram_key(str)
+    key = str_to_char_count_array(str)
     results[key] << str
   end
 
   results.values
 end
 
-def anagram_key(str)
-  str.chars.sort
+def str_to_char_count_array(str)
+  chars = Array.new(26) { 0 }
+  str.each_char { |c| chars[normalize_ascii(c)] += 1 }
+  chars
+end
+
+def normalize_ascii(char)
+  char.ord - 97
 end
