ca934948071de2fea51d174369097460460dcab1
ayrton <ayrton.parkinson1@gmail.com>
2023-06-26 08:13:07 -0400

    Add O(n) solution using bucket sort

--- arrays_and_hashing/top_k_frequent.rb
+++ arrays_and_hashing/top_k_frequent.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 
-# Time complexity: O(n * logn)
+# Time complexity: O(n)
 # Space complexity: O(n)
 def top_k_frequent(nums, k)
-  result = Hash.new(0)
-  nums.each { |n| result[n] += 1 }
+  result = []
+  counts_array = Array.new(nums.length + 1) { [] }
+  counts_hash = Hash.new(0)
+  nums.each { |n| counts_hash[n] += 1 }
 
-  most_frequent = result.max_by(k) { |_k, v| v }
-  most_frequent.map { |kv| kv[0] }
+  counts_hash.each do |k, v|
+    counts_array[v] << k
+  end
+
+  counts_array.reverse_each do |v|
+    next if v.empty?
+
+    result += v
+    break if result.length == k
+  end
+
+  result
 end
