eb2ced2a42083ad124054a4da87fbab1d410c0e0
ayrton <ayrton.parkinson1@gmail.com>
2023-06-28 06:58:21 -0400

    Add solution with lower memory

--- arrays_and_hashing/product_of_array.rb
+++ arrays_and_hashing/product_of_array.rb
@@ -21,3 +21,25 @@ def product_except_self(nums)
 
   prod
 end
+
+# Solution with lower memory
+def product_except_self(nums)
+  length = nums.length
+  prod = Array.new(length)
+
+  0.upto(length - 1).each do |i|
+    prod[i] = if i == 0
+                prod[i] = 1
+              else
+                prod[i] = prod[i - 1] * nums[i - 1]
+              end
+  end
+
+  post = 1
+  (length - 1).downto(0).each do |i|
+    prod[i] *= post
+    post *= nums[i]
+  end
+  
+  prod
+end
