393daf2e4a0d4804cdd8e5b085f81326ca9904f9
ayrton <ayrton.parkinson1@gmail.com>
2023-06-29 07:07:13 -0400

    Add optimized solution

--- two_pointers/two_sum.rb
+++ two_pointers/two_sum.rb
@@ -1,24 +1,15 @@
 # frozen_string_literal: true
 
-# Time complexity: O(n * logn)
+# Time complexity: O(n)
 # Space complexity: O(1)
 def two_sum(numbers, target)
-  numbers.each_with_index do |n, i1|
-    diff = target - n
-    i2 = index(numbers, diff, i1 + 1)
+  i1 = 0
+  i2 = numbers.length - 1
 
-    return [i1 + 1, i2 + 1] if i2
+  while i1 <= i2
+    sum = numbers[i1] + numbers[i2]
+    return [i1 + 1, i2 + 1] if sum == target
+
+    sum > target ? i2 -= 1 : i1 += 1
   end
-end
-
-def index(numbers, target, low = 0, high = numbers.length - 1)
-  while low <= high
-    mid = (low + high) / 2
-    res = numbers[mid]
-    return low + mid if res == target
-
-    res < diff ? low = mid + 1 : high = mid - 1
-  end
-
-  nil
 end
