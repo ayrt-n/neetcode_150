2718fd26a4e9552781a79e6042f48bb7b7e8ab79
ayrton <ayrton.parkinson1@gmail.com>
2023-07-02 09:41:09 -0400

    Refactor solution

--- sliding_window/longest_substring.rb
+++ sliding_window/longest_substring.rb
@@ -3,22 +3,17 @@
 # Time complexity: O(n)
 # Space complexity: O(1)
 def length_of_longest_substring(s)
-  longest, count, l, r = [0, 0, 0, 0]
+  longest, l, r = [0, 0, 0]
   chars = {}
 
   while r < s.length
-    if chars[s[r]]
-      longest = count if count > longest
-      loop do
-        chars[s[l]] = nil
-        count -= 1
-        l += 1
-        break if s[l - 1] == s[r]
-      end
+    while chars[s[r]]
+      chars[s[l]] = nil
+      l += 1
     end
 
     chars[s[r]] = true
-    count += 1
+    longest = [longest, r - l + 1].max
     r += 1
   end
 
