3769e99633b431d6b743832f4096666712e1bf77
ayrton <ayrton.parkinson1@gmail.com>
2023-07-03 13:13:18 -0400

    Refactor to use ruby builtin methods

--- stack/daily_temperatures.rb
+++ stack/daily_temperatures.rb
@@ -7,9 +7,9 @@ def daily_temperatures(temperatures)
   stack = [{ temp: temperatures[0], index: 0 }]
 
   1.upto(temperatures.length - 1) do |i|
-    while !stack.empty? && temperatures[i] > stack[-1][:temp]
-      prev_temp = stack.pop
-      res[prev_temp[:index]] = i - prev_temp[:index]
+    until stack.empty? || temperatures[i] <= stack[-1][:temp]
+      j = stack.pop[:index]
+      res[j] = i - j
     end
     stack << { temp: temperatures[i], index: i }
   end
