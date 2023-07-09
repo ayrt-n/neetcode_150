e3aa1fcb25eb5371de16510720cd15d7638ed364
ayrton <ayrton.parkinson1@gmail.com>
2023-07-09 10:28:04 -0400

    Add optimized solution

--- linked_list/find_duplicate_number.rb
+++ linked_list/find_duplicate_number.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
-# Time complexity: O(n^2)
+# Time complexity: O(n)
 # Space complexity: Problem requirement of O(1)
 # Must not modify original array
-def find_duplicates(nums)
-  (0...nums.length).each do |i|
-    num = nums[i]
-    (i + 1...nums.length).each do |j|
-      return num if num == nums[j]
-    end
+def find_duplicate(nums)
+  slow = nums[nums[0]]
+  fast = nums[nums[nums[0]]]
+
+  # Find the first intersection by iterating slow by one, fast by two
+  until slow == fast
+    slow = nums[slow]
+    fast = nums[nums[fast]]
   end
+
+  # Find the intersection by iterating pointers by one
+  slow = nums[0]
+  until slow == fast
+    slow = nums[slow]
+    fast = nums[fast]
+  end
+
+  # Intersection is the repeated number
+  slow
 end
