5103aa5ff38f3337b07e2954c31b4927c6ab801e
ayrton <ayrton.parkinson1@gmail.com>
2023-07-13 07:49:44 -0400

    Add recursive solution

--- trees/kth_smallest_bst.rb
+++ trees/kth_smallest_bst.rb
@@ -11,13 +11,36 @@ class TreeNode
   end
 end
 
+# ITERATIVE SOLUTION
+# Time complexity: O(n)
+# Space complexity: O(n)
+def kth_smallest(root, k)
+  n = 0
+  stack = []
+  curr = root
+
+  until curr.nil? && stack.empty?
+    while curr
+      stack.push(curr)
+      curr = curr.left
+    end
+
+    curr = stack.pop
+    n += 1
+    return curr.val if n == k
+
+    curr = curr.right
+  end
+end
+
+# RECURSIVE SOLUTION
 # Time complexity: O(n)
 # Space complexity: O(k)
-def kth_smallest(root, k, arr = [])
-  kth_smallest(root.left, k, arr) if root.left
+# def kth_smallest(root, k, arr = [])
+#   kth_smallest(root.left, k, arr) if root.left
 
-  arr.push(root.val) if arr.length < k
-  return arr[-1] if arr.length == k
+#   arr.push(root.val) if arr.length < k
+#   return arr[-1] if arr.length == k
 
-  kth_smallest(root.right, k, arr) if root.right
-end
+#   kth_smallest(root.right, k, arr) if root.right
+# end
