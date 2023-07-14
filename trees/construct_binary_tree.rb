089fad2b59c262f03c97084703d7227a2d9476aa
ayrton <ayrton.parkinson1@gmail.com>
2023-07-14 07:37:25 -0400

    Add build_tree solution

--- trees/construct_binary_tree.rb
+++ trees/construct_binary_tree.rb
@@ -11,8 +11,16 @@ class TreeNode
   end
 end
 
-# Time complexity:
-# Space complexity:
+# Time complexity: O(n^2)
+# Space complexity: O(n)
 def build_tree(preorder, inorder)
+  return nil if preorder.empty? && inorder.empty?
 
+  root = TreeNode.new(preorder[0])
+  pivot = inorder.find_index(preorder[0])
+
+  root.left = build_tree(preorder[1..pivot], inorder[0...pivot])
+  root.right = build_tree(preorder[pivot + 1..], inorder[pivot + 1..])
+  
+  root
 end
