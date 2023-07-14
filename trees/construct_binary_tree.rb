b8295cb04aed62c583230d1850b3d3b333203379
ayrton <ayrton.parkinson1@gmail.com>
2023-07-14 07:36:15 -0400

    Add build_tree solution

---
+++ trees/construct_binary_tree.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Definition for binary tree node
+class TreeNode
+  attr_accessor :val, :left, :right
+
+  def initialize(val)
+    @val = val
+    @left = nil
+    @right = nil
+  end
+end
+
+# Time complexity:
+# Space complexity:
+def build_tree(preorder, inorder)
+
+end
