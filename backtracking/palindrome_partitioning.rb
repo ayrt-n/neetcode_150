dc875db102ee48b14cc96c1bd6ffd0e2ae037f44
ayrton <ayrton.parkinson1@gmail.com>
2023-07-21 14:34:12 -0400

    Add palindrome_partitioning solution

---
+++ backtracking/palindrome_partitioning.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Time complexity:
+# Space complexity:
+def partition(s)
+  backtrack(s, 0)
+end
+
+def backtrack(s, i, res = [], parts = [])
+  res.push(parts.dup) && return if i >= s.length
+
+  (i...s.length).each do |j|
+    next unless palindrome?(s[i..j])
+
+    parts.push(s[i..j])
+    backtrack(s, j + 1, res, parts)
+    parts.pop
+  end
+
+  res
+end
+
+def palindrome?(s)
+  l = 0
+  r = s.length - 1
+
+  while l < r
+    return false if s[l] != s[r]
+
+    l += 1
+    r -= 1
+  end
+
+  true
+end
