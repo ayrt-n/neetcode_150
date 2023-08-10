44da861d88e25521cc5d7937799ad07b0672dd82
ayrton <ayrton.parkinson1@gmail.com>
2023-08-10 07:48:16 -0400

    Refactor solution to improve runtime

--- dynamic_programming/word_break.rb
+++ dynamic_programming/word_break.rb
@@ -1,6 +1,6 @@
 # frozne_string_literal: true
 
-# Time complexity: Still bad
+# Time complexity: O(n * dict)
 # Space complexity: O(1)
 def word_break(s, word_dict)
   recurse(s, word_dict)
@@ -10,9 +10,9 @@ def recurse(s, word_dict, memo = {})
   return true if word_dict.include?(s)
   return false if memo[s] == false
 
-  (0...s.length).each do |i|
-    next unless word_dict.include?(s[0..i])
-    return true if recurse(s[i + 1..], word_dict, memo)
+  word_dict.each do |w|
+    next unless s[0...w.length] == w
+    return true if recurse(s[w.length..], word_dict, memo)
   end
 
   memo[s] = false
