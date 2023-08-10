996c3c2816380ab260b7aa95e25675ed891d0489
ayrton <ayrton.parkinson1@gmail.com>
2023-08-10 07:27:27 -0400

    Improve solution using memoization

--- dynamic_programming/word_break.rb
+++ dynamic_programming/word_break.rb
@@ -1,14 +1,19 @@
 # frozne_string_literal: true
 
-# Time complexity: Real bad
+# Time complexity: Still bad
 # Space complexity: O(1)
 def word_break(s, word_dict)
+  recurse(s, word_dict)
+end
+
+def recurse(s, word_dict, memo = {})
   return true if word_dict.include?(s)
+  return false if memo[s] == false
 
   (0...s.length).each do |i|
     next unless word_dict.include?(s[0..i])
-    return true if word_break(s[i + 1..], word_dict)
+    return true if recurse(s[i + 1..], word_dict, memo)
   end
 
-  false
+  memo[s] = false
 end
