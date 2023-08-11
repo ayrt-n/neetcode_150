e8c66a581484217873a49e4502686a9febcef9e3
ayrton <ayrton.parkinson1@gmail.com>
2023-08-11 07:58:53 -0400

    Add time and space complexity comment

--- dynamic_programming/longest_increasing_subsequence.rb
+++ dynamic_programming/longest_increasing_subsequence.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# Time complexity:
-# Space complexity:
+# Time complexity: O(n^2)
+# Space complexity: O(n)
 def length_of_list(nums)
   dp = Array.new(nums.length) { 1 }
 
@@ -10,32 +10,6 @@ def length_of_list(nums)
       dp[i] = [dp[i], dp[j] + 1].max if nums[j] > nums[i]
     end
   end
+
+  dp.max
 end
-
-
-# def length_of_lis(nums)
-#   res = 1
-#   memo = {}
-
-#   (0...nums.length).each do |i|
-#     res = [res, recurse(nums, i, memo)].max
-#   end
-
-#   res
-# end
-
-# def recurse(nums, index, memo, count = 1)
-#   if index >= nums.length
-#     memo[index] = count
-#     return count
-#   end
-
-#   res = count
-#   (index + 1...nums.length).each do |i|
-#     if nums[i] > nums[index]
-#       res = [res, recurse(nums, i, count + 1)].max
-#     end
-#   end
-
-#   res
-# end
