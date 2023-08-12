2b21e5b667397aa20aa60bb70554a590698678b5
ayrton <ayrton.parkinson1@gmail.com>
2023-08-12 15:14:47 -0400

    Add dynamic programming soluton

--- dynamic_programming/partition_equal_subset.rb
+++ dynamic_programming/partition_equal_subset.rb
@@ -1,23 +1,24 @@
 # frozen_string_literal: true
 
-# Time complexity: ~O(2^n)
+# Time complexity: O(sum(nums))
 def can_partition(nums)
-  dfs(nums)
-end
-
-def dfs(nums, index = 0, sum1 = 0, sum2 = 0, memo = {})
-  return sum1 == sum2 if index >= nums.length
+  sum = nums.sum
+  return false if nums.sum % 2 == 1
   
-  set = Set.new([sum1, sum2])
-  return memo[set] if memo.has_key?(set)
+  dp = Set.new([0])
+  target = sum / 2
+  (nums.length - 1).downto(0) do |i|
+    tmp = Set.new
 
-  set1 = Set.new([sum1 + nums[index], sum2]) 
-  memo[set1] = dfs(nums, index + 1, sum1 + nums[index], sum2, memo)
-  return true if memo[set1]
+    dp.each do |n|
+      new_sum = n + nums[i]
+      return true if new_sum == target
 
-  set2 = Set.new([sum1, sum2 + nums[index]]) 
-  memo[set2] = dfs(nums, index + 1, sum1, sum2 + nums[index], memo)
-  return true if memo[set2]
+      tmp << new_sum
+    end
+
+    dp += tmp
+  end
 
   false
 end
