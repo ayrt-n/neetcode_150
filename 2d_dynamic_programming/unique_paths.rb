e7ca8edbcd97e03c5aac1eff08e51f0eb196b43c
ayrton <ayrton.parkinson1@gmail.com>
2023-08-13 08:10:02 -0400

    Add bottom up DP solution

--- 2d_dynamic_programming/unique_paths.rb
+++ 2d_dynamic_programming/unique_paths.rb
@@ -4,16 +4,19 @@
 # Space complexity: O(m * n)
 def unique_paths(m, n)
   grid = Array.new(m) { Array.new(n) }
-  grid[m - 1][n - 1] = 1
+  last_row, last_col = [m - 1, n - 1]
 
-  dfs(grid, m, n)
-end
+  (grid.length - 1).downto(0) do |r|
+    (grid[0].length - 1).downto(0) do |c|
+      if r == last_row || c == last_col
+        grid[r][c] = 1
+      else
+        grid[r][c] = 0
+        grid[r][c] += grid[r + 1][c] || 0
+        grid[r][c] += grid[r][c + 1] || 0
+      end
+    end
+  end
 
-def dfs(grid, m, n, row = 0, col = 0)
-  return 0 if row >= m || col >= n
-  return grid[row][col] if grid[row][col]
-
-  paths = 0
-  paths += dfs(grid, m, n, row, col + 1)
-  paths += dfs(grid, m, n, row + 1, col)
+  grid[0][0]
 end
