5119bcfada5f9a39620a86e082d85bbdb98630de
ayrton <ayrton.parkinson1@gmail.com>
2023-08-22 07:58:52 -0400

    Add dynamic programming soluton

--- 2d_dynamic_programming/target_sum.rb
+++ 2d_dynamic_programming/target_sum.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 # DFS + Memoization Solution
-# Time complexity: O(nums)
-# Space complexity: O(nums)
+# Time complexity: O(nums * sum)
+# Space complexity: O(nums * sum)
 def find_target_sum_ways(nums, target)
   dfs(nums, 0, target, 0)
 end
@@ -15,4 +15,24 @@ def dfs(nums, index, target, sum, cache = {})
   cache[[index, sum]] = 0
   cache[[index, sum]] += dfs(nums, index + 1, target, sum + nums[index], cache)
   cache[[index, sum]] += dfs(nums, index + 1, target, sum - nums[index], cache)
+end
+
+# Dynamic Programming Solution
+# Time complexity: O(nums * sum)
+# Space complexity: O(nums * sum)
+def find_target_sum_ways(nums, target)
+  sum = nums.sum
+  dp = Array.new(nums.length + 1) { Hash.new { |h, k| h[k] = 0 } }
+  dp[-1][target] = 1
+
+  (nums.length - 1).downto(0) do |i|
+    (-sum).upto(sum) do |j|
+      a = j + nums[i]
+      m = j - nums[i]
+
+      dp[i][j] += dp[i + 1][a] + dp[i + 1][m]
+    end
+  end
+
+  dp[0][0]
 end
