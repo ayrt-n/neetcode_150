1213ee4a1c49caf16ae7787556ea1ec4c24cab04
ayrton <ayrton.parkinson1@gmail.com>
2023-08-23 07:23:58 -0400

    Refactor solution

--- 2d_dynamic_programming/interleaving_string.rb
+++ 2d_dynamic_programming/interleaving_string.rb
@@ -8,14 +8,8 @@ end
 
 def dfs(s1, s2, s3, p1 = 0, p2 = 0, p3 = 0)
   return p1 == s1.length && p2 == s2.length if p3 == s3.length
-
-  if s1[p1] == s3[p3]
-    return true if dfs(s1, s2, s3, p1 + 1, p2, p3 + 1)
-  end
-
-  if s2[p2] == s3[p3]
-    return true if dfs(s1, s2, s3, p1, p2 + 1, p3 + 1)
-  end
+  return true if s1[p1] == s3[p3] && dfs(s1, s2, s3, p1 + 1, p2, p3 + 1)
+  return true if s2[p2] == s3[p3] && dfs(s1, s2, s3, p1, p2 + 1, p3 + 1)
 
   false
 end
