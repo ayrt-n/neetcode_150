9385af722750d68ed3f49e81e4dbffbc6cf6929f
ayrton <ayrton.parkinson1@gmail.com>
2023-08-23 07:02:59 -0400

    Add brute force solution

---
+++ 2d_dynamic_programming/interleaving_string.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Time complexity:
+# Space complexity:
+def is_interleave(s1, s2, s3)
+  dfs(s1, s2, s3)
+end
+
+def dfs(s1, s2, s3, p1 = 0, p2 = 0, p3 = 0)
+  return p1 == s1.length && p2 == s2.length if p3 == s3.length
+
+  if s1[p1] == s3[p3]
+    return true if dfs(s1, s2, s3, p1 + 1, p2, p3 + 1)
+  end
+
+  if s2[p2] == s3[p3]
+    return true if dfs(s1, s2, s3, p1, p2 + 1, p3 + 1)
+  end
+
+  false
+end
