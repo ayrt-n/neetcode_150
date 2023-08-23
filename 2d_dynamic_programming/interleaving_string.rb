ad18856b13aa5bab7841ff16096c6796dcf9a636
ayrton <ayrton.parkinson1@gmail.com>
2023-08-23 08:17:30 -0400

    Add dynamic programming solution

--- 2d_dynamic_programming/interleaving_string.rb
+++ 2d_dynamic_programming/interleaving_string.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-# Time complexity:
-# Space complexity:
+# DFS + Memoization Solution
+# Time complexity: O(m * n)
+# Space complexity: O(m * n)
 def is_interleave(s1, s2, s3)
   dfs(s1, s2, s3)
 end
@@ -19,4 +20,25 @@ def dfs(s1, s2, s3, p1 = 0, p2 = 0, p3 = 0, cache = {})
   end
 
   cache[[p1, p2]] = false
+end
+
+# Dynamic Programming Solution
+# Time complexity: O(m * n)
+# Space complexity: O(m * n)
+def is_interleave(s1, s2, s3)
+  return false if s1.length + s2.length != s3.length
+
+  dp = Array.new(s1.length + 1) { Array.new(s2.length + 1) { false } }
+  dp[-1][-1] = true
+  
+  (dp.length - 1).downto(0) do |p1|
+    (dp[p1].length - 1).downto(0) do |p2|
+      next if dp[p1][p2]
+
+      dp[p1][p2] = dp[p1 + 1][p2] if s1[p1] == s3[p1 + p2]
+      dp[p1][p2] ||= dp[p1][p2 + 1] if s2[p2] == s3[p1 + p2]
+    end
+  end
+  
+  dp[0][0]
 end
