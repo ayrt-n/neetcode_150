ed6932c8756b1d10367d2dfa3e087bbab6cad3a7
ayrton <ayrton.parkinson1@gmail.com>
2023-08-26 10:28:05 -0400

    Add greedy solution

--- greedy/gas_sation.rb
+++ greedy/gas_sation.rb
@@ -3,16 +3,19 @@
 # Time complexity: O(n)
 # Space complexity: O(1)
 def can_complete_circuit(gas, cost)
-  (0...gas.length).each do |i|
-    tank = 0
-    (0...gas.length).each do |j|
-      index = (i + j) % gas.length
-      tank += gas[index]
-      tank -= cost[index]
-      break if tank.negative?
-    end
+  return -1 if gas.sum < cost.sum
 
-    return i if tank >= 0
+  total = 0
+  start = 0
+
+  (0...gas.length).each do |i|
+    total += gas[i] - cost[i]
+
+    if total.negative?
+      start = i + 1
+      total = 0
+    end
   end
-  -1
+
+  start
 end
