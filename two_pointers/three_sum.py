9ab8422c6487cc451992e2f7ee3957e432824283
ayrton <ayrton.parkinson1@gmail.com>
2024-05-13 07:21:02 -0400

    Add optimal solution

--- two_pointers/three_sum.py
+++ two_pointers/three_sum.py
@@ -1,4 +1,29 @@
 class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        length = len(nums)
+        nums.sort()
+
+        for i, ni in enumerate(nums):
+            if i > 0 and nums[i - 1] == ni:
+                continue
+
+            l, r = i + 1, length - 1
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while (nums[l - 1] == nums[l] and l < r):
+                        l += 1
+
+        return res
+    
     def threeSumNaive(self, nums: List[int]) -> List[List[int]]:
         res = []
         incl = set()
